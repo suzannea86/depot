3896226e6b3aa62003dd82b2cdd8e5c13eaf7af1
suzannea86 <suzanne.a86@gmail.com>
2013-07-09 16:11:54 +0800

    Playtime changes until Task D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
     @products = Product.all
     @cart = current_cart
+    @count = increment_counter
+  end
+
+  def increment_counter
+  	if session[:counter].nil?
+  		session[:counter] = 0
+  	else
+  		session[:counter] += 1
+  	end
   end
 end
