a070ad719ad26fb72f7df2f697da69ea3d469834
suzannea86 <suzanne.a86@gmail.com>
2013-07-10 14:32:55 +0800

    Task D and Task E(in progress) Playtime

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,7 +47,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        @count = 0 
+        session[:counter] = nil
         format.html { redirect_to(store_url) }
         format.js {@current_item = @line_item}
         format.xml { render xml: @line_item, status: :created, location: @line_item }
@@ -78,10 +78,12 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     @line_item = LineItem.find(params[:id])
+    @cart = @line_item.cart
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to store_url }
+      format.js
       format.json { head :no_content }
     end
   end
