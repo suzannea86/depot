67c065712e94df196383929f3f2cdb5bad99e2ab
suzannea86 <suzanne.a86@gmail.com>
2013-07-18 11:46:14 +0800

    Passed all tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
 
   validates :name, :address, :email, :pay_type, :presence => true
-  validates :pay_type, :inclusion => ["Check", "Purchase Order", "Credit Card"]
+  validates :pay_type, :inclusion => PaymentType.pay_types
   def add_line_items_from_cart(cart)
   	cart.line_items.each do |item|
   		item.cart_id = nil
