e809ffa0992ad50f886eed9aa6d11eb55cdba137
marilyn <marilyn.aseer@gmail.com>
2013-07-19 15:13:59 +0800

    Adding Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130716081430) do
+ActiveRecord::Schema.define(:version => 20130718035211) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(:version => 20130716081430) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
